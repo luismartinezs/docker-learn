46e7c870e3d0dcc5f598f00b42ba69f29ed73770
Luis Martinez <luismartinezwebdev@gmail.com>
2024-10-04 13:12:22 +0700

    working dev and prod docker

--- basic-vue-app/src/components/HelloWorld.vue
+++ basic-vue-app/src/components/HelloWorld.vue
@@ -9,6 +9,8 @@ const count = ref(0)
 <template>
   <h1>{{ msg }}</h1>
 
+  <p>Hello, I am editing this file</p>
+
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
