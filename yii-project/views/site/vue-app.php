165bdf25007b55ba38187cbaca65039799bcb4da
Luis Martinez <luismartinezwebdev@gmail.com>
2024-10-05 13:07:11 +0700

    vue from cdn

--- yii-project/views/site/vue-app.php
+++ yii-project/views/site/vue-app.php
@@ -1,3 +1,17 @@
+<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 <div>
   <p>This is a Vue app embedded in a Yii project</p>
+  <div id="app">{{ message }}</div>
 </div>
+<script>
+  const { createApp, ref } = Vue
+
+  createApp({
+    setup() {
+      const message = ref('Hello vue!')
+      return {
+        message
+      }
+    }
+  }).mount('#app')
+</script>
