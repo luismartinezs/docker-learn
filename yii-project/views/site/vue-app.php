6d02efeb6e105604fa3e3bcbd1d6064d92592e9f
Luis Martinez <luismartinezwebdev@gmail.com>
2024-10-05 13:30:39 +0700

    vue widget working

--- yii-project/views/site/vue-app.php
+++ yii-project/views/site/vue-app.php
@@ -1,17 +1,13 @@
+<!-- load vue script -->
 <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+<!-- add script to mount vue app at the end of the body -->
+<?php
+$this->registerJsFile('@web/js/vue-widget.js', ['position' => \yii\web\View::POS_END]);
+?>
+
 <div>
   <p>This is a Vue app embedded in a Yii project</p>
-  <div id="app">{{ message }}</div>
+  <div id="vue-widget">
+     {{message}}
+  </div>
 </div>
-<script>
-  const { createApp, ref } = Vue
-
-  createApp({
-    setup() {
-      const message = ref('Hello vue!')
-      return {
-        message
-      }
-    }
-  }).mount('#app')
-</script>
